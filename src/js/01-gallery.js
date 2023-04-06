4762da20f23911a6f6a348a2a0ec4f511bd7ee94
luftboud <ijamaharyta@gmail.com>
2023-04-06 18:48:18 +0300

    task 2 - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,10 +6,10 @@ import { galleryItems } from "./gallery-items.js";
 //add markup
 const list = document.querySelector(".gallery");
 (function () {
-    const markup = galleryItems
-      .map(
-        ({ id, preview, original, description }) =>
-          `<li class="gallery__item" data-id="${id}">
+  const markup = galleryItems
+    .map(
+      ({ id, preview, original, description }) =>
+        `<li class="gallery__item" data-id="${id}">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -19,29 +19,29 @@ const list = document.querySelector(".gallery");
     />
   </a>
 </li>`
-      )
-      .join("");
-    list.insertAdjacentHTML('beforeend', markup);
+    )
+    .join("");
+  list.insertAdjacentHTML("beforeend", markup);
 })();
 
 //add listener
-list.addEventListener('click', onClick);
+list.addEventListener("click", onClick);
 
 function onClick(event) {
-    event.preventDefault();
-  
-    if (!event.target.classList.contains('gallery__image')) {
-      return;
-    }
-    const currentItem = event.target.closest(".gallery__item");
-    const imgId = Number(currentItem.dataset.id);
-    const data = galleryItems.find(({ id }) => id === imgId);
-    const instance = basicLightbox.create(
-      `
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const currentItem = event.target.closest(".gallery__item");
+  const imgId = Number(currentItem.dataset.id);
+  const data = galleryItems.find(({ id }) => id === imgId);
+  const instance = basicLightbox.create(
+    `
         <div>
             <img src="${data.original}" alt="${data.description}">
         </div>
     `
-    );
-    instance.show();
+  );
+  instance.show();
 }
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//add markup
+const list = document.querySelector(".gallery");
+(function () {
+  const markup = galleryItems
+    .map(
+      ({ id, preview, original, description }) =>
+        `<li class="gallery__item" data-id="${id}">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+      .join("");
+      list.insertAdjacentHTML("beforeend", markup);
+    })();
+    
+    
+const galleryLightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+    captionDelay: 250
+});
